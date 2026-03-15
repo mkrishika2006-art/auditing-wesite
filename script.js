9d46a716cf59428cbc27b6b4734eb0d1a2b5f116
mkrishika2006-art <mkrishika2006@gmail.com>
2026-03-15 19:12:31 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function togglemenu(){
-    const menu=document.querySelector(".menu-links");
-    const icon=document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+anchor.addEventListener('click', function (e) {
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+behavior: 'smooth'
+});
+
+});
+
+});
